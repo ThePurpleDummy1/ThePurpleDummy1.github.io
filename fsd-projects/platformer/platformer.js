689174567799175c0ed242b84082d3e139a97610
Chris <christopher.zane73@gmail.com>
2025-10-08 20:52:00 +0000

    Update

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -105,3 +105,14 @@ bgm4.pause();
 }
 };
 
+var bgm5 = new Audio("cornered.ogg"); // replace with the actual path to your mp3
+bgm5.loop = true; // keeps playing in a loop
+bgm5.volume = 0.2; // optional: volume between 0.0 and 1.0
+
+window.playAudio5 = function () {
+if (bgm5.paused) {
+bgm5.play();
+} else {
+bgm5.pause();
+}
+};
